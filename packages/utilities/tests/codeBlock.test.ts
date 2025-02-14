9e1514effd196c21e725a577401303caa0fe47f1
Pauline <git@ethanlibs.co>
2025-02-14 17:24:15 +0100

    ✅ test(codeBlock): add more typesafe testing
    Signed-off-by: Pauline <git@ethanlibs.co>

--- packages/utilities/tests/codeBlock.test.ts
+++ packages/utilities/tests/codeBlock.test.ts
@@ -6,13 +6,13 @@ describe('codeBlock', () => {
 	it('given expression w/o length then returns wrapped ZeroWidthSpace', () => {
 		expect(codeBlock('md', '')).toStrictEqual(`\`\`\`md
 
-\`\`\``);
+\`\`\`` as const);
 	});
 
 	it('given expression w/ length then returns expressed wrapped in markdown', () => {
 		expect(codeBlock('md', '# Header')).toStrictEqual(`\`\`\`md
 # Header
-\`\`\``);
+\`\`\`` as const);
 	});
 
 	it('given expression w/ length w/ inner code block then returns expressed wrapped in markdown', () => {
@@ -34,7 +34,7 @@ describe('codeBlock', () => {
 				if (flowr) return 'awesome!';
 			\`\`\`
 		
-\`\`\``,
+\`\`\`` as const,
 		);
 	});
 
@@ -52,20 +52,20 @@ describe('codeBlock', () => {
 			# Header
 			\`const flowr = true\`
 		
-\`\`\``);
+\`\`\`` as const);
 	});
 
 	it('given expression of non-string type then returns wrapped expression', () => {
 		// @ts-expect-error: Checking for invalid input
 		expect(codeBlock('md', 123456789)).toStrictEqual(`\`\`\`md
 123456789
-\`\`\``);
+\`\`\`` as const);
 	});
 
 	it('given expression of falsey type then returns wrapped expression', () => {
 		// @ts-expect-error: Checking for invalid input
 		expect(codeBlock('md', false)).toStrictEqual(`\`\`\`md
 false
-\`\`\``);
+\`\`\`` as const);
 	});
 });
